60b2430a99fe9999ee33ff25ae183190ab01c150
britgh <87544095+britgh@users.noreply.github.com>
2025-05-27 01:51:22 -0700

    min / max functions added

--- K-Nearest_Neighbors/processing.py
+++ K-Nearest_Neighbors/processing.py
@@ -2,13 +2,19 @@
 import numpy as np
 
 # min-max normalization
-def normalize(current, maximum, minimum) :                  # max-min data normalization
-    return (current - minimum) / (maximum - minimum)
+def normalize(current, maxx, minn) :
+    return (current - minn) / (maxx - minn)
 
 # Euclidean distance
 def euclid_dist(training, testing) :
     return np.sqrt(np.sum(np.square(training - testing), axis = 1))
 
+# finding the max and min
+def maximum(current, max) :
+    return current if current > max else max
+
+def minimum(current, min) :
+    return current if current < min else min
 
 # finding mode
 # def mode (items) :
