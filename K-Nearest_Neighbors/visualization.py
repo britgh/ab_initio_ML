9e760a6c4e207f2fbff89ba3ec543cadd35f1223
britgh <87544095+britgh@users.noreply.github.com>
2025-05-27 19:22:31 -0700

    custom graph legend

--- K-Nearest_Neighbors/visualization.py
+++ K-Nearest_Neighbors/visualization.py
@@ -2,6 +2,8 @@
 # britgh last updated: 05.27.25
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.lines import Lines2D
 import numpy as np
 
 def plot (train_set, test_set, train_label, all) :
@@ -19,19 +21,32 @@ def plot (train_set, test_set, train_label, all) :
     train_label = train_label.replace({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor': 2}).infer_objects(copy=False)
     # print(test_label)
 
-    # altering visual elements
+    # custom legend
     colors = np.array(['r', 'g'])
+    grey_guide = patches.Patch(color='gray', label='training data')
+    green_guide = patches.Patch(color='green', label = 'true predict')
+    red_guide = patches.Patch(color='red', label='false predict')
+
+    # custom icons
     icons = np.array(['o', 's', '^'])
+    circle_icon = Lines2D([0,0], label='Iris-setosa', c='black', marker='o')
+    square_icon = Lines2D([0,0], label='Iris-virginica', c='black', marker='s')
+    triangle_icon = Lines2D([0,0], label='Iris-versicolor', c='black', marker='^')
+
+    plt.legend(handles=[grey_guide, green_guide, red_guide, circle_icon, square_icon, triangle_icon])
 
     # plot training data
-    print(train_label)
+    # print(train_label)
     for row in range(len(train_set)):
         # print(train_label.iloc[row].get('class'))
+        # print(train_set)
         axes.scatter(train_set.iloc[row, 0], train_set.iloc[row, 1], train_set.iloc[row, 2],
                      c='gray', marker=icons[train_label.iloc[row].get('class')])
 
     # plot testing data
     for row in range(len(test_set)):
+        # print(test_set)
+        # print(test_label)
         axes.scatter(test_set.iloc[row, 0], test_set.iloc[row, 1], test_set.iloc[row, 2],
                      c=colors[test_label.iloc[row,2]], marker=icons[test_label.iloc[row,1]]) # markers reflect actual label (not pred)
 
