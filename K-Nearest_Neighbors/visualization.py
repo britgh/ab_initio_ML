96294bd507bd0ce40c0e52cd54fbc3551c4ce168
britgh <87544095+britgh@users.noreply.github.com>
2025-05-27 18:56:24 -0700

    graph working, check weird groupings

--- K-Nearest_Neighbors/visualization.py
+++ K-Nearest_Neighbors/visualization.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot (train_set, test_set, train_label, all) :
-    print(all)
+    # print(all)
     graph = plt.figure()
     axes = graph.add_subplot(111, projection='3d')  # 3D graph for 3 features
 
@@ -15,7 +15,7 @@ def plot (train_set, test_set, train_label, all) :
     axes.set_zlabel('Z Axis: Petal Length')
 
     # encode classes, allow downcasting (string -> int)
-    label_set = all.replace({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor': 2}).infer_objects(copy=False)
+    test_label = all.replace({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor': 2}).infer_objects(copy=False)
     train_label = train_label.replace({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor': 2}).infer_objects(copy=False)
     # print(test_label)
 
@@ -24,13 +24,15 @@ def plot (train_set, test_set, train_label, all) :
     icons = np.array(['o', 's', '^'])
 
     # plot training data
+    print(train_label)
     for row in range(len(train_set)):
+        # print(train_label.iloc[row].get('class'))
         axes.scatter(train_set.iloc[row, 0], train_set.iloc[row, 1], train_set.iloc[row, 2],
-                     c='gray', marker=icons[train_label.astype(int)[row]])
+                     c='gray', marker=icons[train_label.iloc[row].get('class')])
 
     # plot testing data
     for row in range(len(test_set)):
         axes.scatter(test_set.iloc[row, 0], test_set.iloc[row, 1], test_set.iloc[row, 2],
-                     c=colors[label_set[:,2].astype(int)[row]], marker=icons[label_set[:,1].astype(int)[row]]) # markers reflect actual label (not pred)
+                     c=colors[test_label.iloc[row,2]], marker=icons[test_label.iloc[row,1]]) # markers reflect actual label (not pred)
 
     plt.show()
