gradient descent / derivatives should be correct

Multi-Linear_Regression/calculations.py
@@ -20,19 +20,22 @@ def mse (given_x, weights, bias, real_y) :
 
     predict = predict_y(given_x, weights, bias)
 
-    return (np.sum(np.square(real_y - predict), axis=0)) / cases
+    return (np.sum(np.square(real_y - predict), axis=0)).values / cases
 
 # returns partial derivatives w.r.t weights + bias
 def partial_deriv(given_x, weights, bias, real_y) :
-    cases = given_x.shape[0]
-
-    # features = given_x.shape[1]
-    # w_deriv = np.zeros(features)        # set default vals
-    # b_deriv = 0
+    w_deriv = np.zeros(given_x.shape[1])        # set default vals
+    b_deriv = 0
 
     # partial derivatives w.r.t weights + bias
-    w_deriv = np.sum((predict_y(given_x, weights, bias) - real_y).values * given_x * 2, axis=1) / cases
-    b_deriv = np.sum((predict_y(given_x, weights, bias) - real_y).values * 2, axis=1) / cases
+    difference = (predict_y(given_x, weights, bias) - real_y).values
+
+    for row in range(difference.shape[0]):
+        b_deriv += difference[row]
+        w_deriv += (difference[row] * given_x.iloc[row]).values
+
+    w_deriv /= difference.shape[0]
+    b_deriv /= difference.shape[0]
 
     return w_deriv, b_deriv
 

Multi-Linear_Regression/linear_regression.py
@@ -1,5 +1,5 @@
 # Multi-Linear Regression Machine Learning Model
-# britgh last updated: 08.20.25
+# britgh last updated: 08.23.25
 
 import calculations
 import correlation
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from ucimlrepo import fetch_ucirepo                         # MPG dataset by UCIML
 from sklearn.model_selection import KFold                   # help implement K-Fold cross-validation
+import matplotlib.pyplot as plt
 
 # DATASET---
 auto_mpg = fetch_ucirepo(id=9)
@@ -31,18 +32,30 @@ for train_index, test_index in KFold(n_splits=10).split(X):             # iterat
     X_training, X_testing = X.iloc[train_index], X.iloc[test_index]     # indices
     Y_training, Y_testing = Y.iloc[train_index], Y.iloc[test_index]
 
-    # MODEL TRAINING--- find m + b s.t. mse is minimized
+    # MODEL TRAINING---
+    learning_rate = 0.000001                                               # default stuff
+    weights = np.zeros(X_training.shape[1])
+    bias = 0
+
+    print("\nBefore GD:", calculations.mse(X_training, weights, bias, Y_training))
+    w_deriv, b_deriv = calculations.partial_deriv(X_training, weights, bias, Y_training)
+
+    print("W deriv:", w_deriv)
+    print("B deriv:", b_deriv)
+
+    w_new = weights - learning_rate * w_deriv
+    b_new = bias - learning_rate * b_deriv
+
+    print("new W:", w_new)
+    print("new B:", b_new)
+
+    print("After GD:", calculations.mse(X_training, w_new, b_new, Y_training))
 
 
 
 
-    # values = np.empty((2, 3))  # stores training sd + mean
-    # calculations.z_score(values, True, X, column)
 
-
-
-
-
+    # do the zscore thing
 
 
     # MODEL TESTING--- find predicted y according to line